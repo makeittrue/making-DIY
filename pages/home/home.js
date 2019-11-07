7268a33d395c17d8d928b512d99c2836eebd8a26
hmy <software_hb@163.com>
2019-11-07 22:18:30 +0800

    11.7

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    email: 'software_hb@163.com',
+    version: 'v.bate0.0.1',
+    libVersion: 'bate0.0.1'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let userInfo = wx.getStorageSync('userInfo')
+    if (userInfo.nickName) {
+      this.setData({
+        userInfo
+      })
+    } else {
+      // 查看是否授权
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo({
+              success: res => {
+                this.setData({
+                  userInfo: res.userInfo
+                })
+              }
+            })
+          }
+        }
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onGotUserInfo(e) {
+    let userInfo = e.detail.userInfo
+    wx.setStorageSync('userInfo', userInfo)
+    this.setData({
+      userInfo
+    })
   }
 })
